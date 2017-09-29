3fcb460b4d7466e4b975b151ea6b3082efd674c3
Sam Ojling <s.ojling@gmail.com>
2017-09-28 22:25:32 -0700

    Cleaned up some comments, made change to a float num on instantiation, created sort and list sort methods

--- lib/crypto_market/coin.rb
+++ lib/crypto_market/coin.rb
@@ -1,5 +1,5 @@
 # Crypto Coin belongs to Crypto Currencies
-# Responsible for handling all of the Crypto Coin informatio
+# Responsible for handling all of the Crypto Coin information
 
 class CryptoMarket::Coin
   attr_accessor :name, :price, :change
@@ -7,10 +7,10 @@ class CryptoMarket::Coin
   def initialize(name, price, change)
     @name = name
     @price = price
-    @change = change
+    @change = change.to_f
   end
 
-  # Prints out the coin  value in different currencies
+  # Prints out the coin value in different currencies
   def currency_list
     price.select { |currency, price| puts "#{currency.upcase} #{price.to_f.round(2)}" }
   end
--- lib/crypto_market/currencies.rb
+++ lib/crypto_market/currencies.rb
@@ -1,16 +1,14 @@
 # Crypto Currencies is having many Crypto Coins
-# Ranking all the coins
 
 class CryptoMarket::Currencies
   attr_accessor :coins
-  # This over Constant, currencies might change?
   # Store all the currencies in an Array
   def initialize
     @coin_data = CryptoMarket::Api
     @coins = []
   end
 
-  # Instantiates new coins from Coin class with selected properties
+  # Instantiates new coins from Coin class with attributes specified from coin_attribues method
   def run
     create_coins_from_attributes
   end
@@ -32,7 +30,7 @@ class CryptoMarket::Currencies
     end
   end
 
-  # Returns hash with symbol, position, price and change for each coin
+  # Returns an Array with hashes containing information specified below for each coin
   def coin_attributes
     @coin_data.fetch_coin_data.map do |coin_name, coin_attributes|
       coin_attributes.select do |attribute, attribute_value|
@@ -41,17 +39,31 @@ class CryptoMarket::Currencies
     end
   end
 
+  # Returns an instance of Coin for the specific coin that user is looking for
+  def find_name(index)
+    coins[index - 1]
+  end
+
   # Ranks the coins from 0 - user input, if no input default to all of the coins
   def sort_by_price(index = coins.length)
     coins.sort_by { |coin| coin.price['usd'] }.reverse[0...index]
   end
 
-  # Ranks the coins from 0 - user input, if no input default to all of the coins
-  def sort_by_change(option, index = coins.length)
-    if option == 'positive'
-      coins.sort_by { |coin| coin.change.to_i }.reverse[0...index]
-    elsif option == 'negative'
-      coins.sort_by { |coin| coin.change.to_i }[0...index]
+  # Return sorted Array based on user input (positive, negative or default)
+  def sort_by_change(input, range)
+    if input == '+'
+      coins.select { |coin| coin.change > 0 }.sort_by { |coin| coin.change }.reverse[0...range]
+    elsif input == '-'
+      coins.select { |coin| coin.change < 0 }.sort_by { |coin| coin.change }[0...range]
+    else
+      coins.sort_by { |coin| coin.change }.reverse[0...range]
+    end
+  end
+
+  # Prints out the changes based on positive, negative or default input, also possible to select top(number)
+  def list_change(input = nil, range = coins.length)
+    sort_by_change(input, range).each_with_index do |coin, index|
+      puts "#{index + 1}. #{coin.name} (#{input if input == '+'}#{coin.change}%)"
     end
   end
 
@@ -60,11 +72,6 @@ class CryptoMarket::Currencies
     coins.each_with_index do |coin, index|
       puts "#{index + 1}. #{coin.name}"
     end
-  end
-
-  # Returns hash for specific coin that user is looking for
-  def find_name(index)
-    coins[index - 1]
   end
 
 end
